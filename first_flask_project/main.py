5ddb1705324b2a7e3637936bd7a49c5dbdea0fbf
eshwar2120 <69860091+eshwar2120@users.noreply.github.com>
2022-10-29 22:16:28 +0530

    First commit

---
+++ first_flask_project/main.py
@@ -0,0 +1,61 @@
+from flask import Flask,redirect,url_for,render_template,request,make_response
+import ibm_db
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=764264db-9824-4b7c-82df-40d1b13897c2.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32536;SECURITY=SSL;SSLServerCertificate=abc.crt;UID=gnq12618;PWD=0glS4tFaR2ciK8fB",'','')
+print(conn)
+print("connection successful...")
+app = Flask(__name__)
+
+
+
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        sql = "select * from user where username=? and password=?"
+        stmt = ibm_db.prepare(conn,sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        dic = ibm_db.fetch_assoc(stmt)
+        print(dic)
+        if dic:
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+        return redirect(url_for('home'))
+    elif request.method=='GET':
+        return render_template('login.html')
+
+@app.route('/signup',methods=['POST','GET'])
+def signup():
+    if request.method=='POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        roll_no = request.form['roll_no']
+        sex = request.form['sex']
+        age = request.form['age']
+        address = request.fomr['address']
+        blood_group = request.fomr['blood_group']
+        sql = "insert into user values(?,?,?,?)"
+        prep_stmt = ibm_db.prepare(conn,sql)
+        ibm_db.bind_param(prep_stmt,1,email)
+        ibm_db.bind_param(prep_stmt,2,username)
+        ibm_db.bind_param(prep_stmt,3,roll_no)
+        ibm_db.bind_param(prep_stmt,4,password)
+        ibm_db.execute(prep_stmt)
+        #db post operation
+        return redirect(url_for('login'))
+    elif request.method=='GET':
+        return render_template('signup.html')
+
+
+if __name__=='__main__':
+    app.run(debug = True)
